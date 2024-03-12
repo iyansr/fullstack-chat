27a311b26b1e3424ae940ced468b12385117ef64
I Putu Saputrayana <ikhyan7@gmail.com>
2024-03-13 01:06:15 +0800

    add production api

--- mobile/modules/config/index.ts
+++ mobile/modules/config/index.ts
@@ -1,9 +1,3 @@
-const devConfig = {
-  API_URL: 'http://192.168.0.104:8765/v1',
+export const config = {
+  API_URL: 'https://fullstack-chat.iyansr.id/v1',
 };
-
-const productionConfig = {
-  API_URL: 'http://localhost:8765/v1',
-};
-
-export const config = __DEV__ ? devConfig : productionConfig;
