e7094313e61b3eb72931f0f4d66807270cb00d33
I Putu Saputrayana <ikhyan7@gmail.com>
2024-03-19 20:52:37 +0800

    improve logger

--- server/src/application/logger.ts
+++ server/src/application/logger.ts
@@ -1,7 +1,26 @@
 import winston from 'winston';
 
+const alignColorsAndTime = winston.format.combine(
+  winston.format.colorize({
+    all: true,
+  }),
+  winston.format.label({
+    label: '[LOGGER]',
+  }),
+  winston.format.timestamp({
+    format: 'YY-MM-DD HH:mm:ss',
+  }),
+  winston.format.printf(
+    (info) => `${info.label}  ${info.timestamp} [${info.level}] : ${info.message}`,
+  ),
+);
+
 export const logger = winston.createLogger({
   level: 'debug',
   format: winston.format.json(),
-  transports: [new winston.transports.Console({})],
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.colorize(), alignColorsAndTime),
+    }),
+  ],
 });
